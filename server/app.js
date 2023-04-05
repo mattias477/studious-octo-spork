f22ecb2cbbdad7a8aa23fcd2ef14cb391dfc7bee
mattias477 <olssonmattias477@gmail.com>
2023-04-04 20:43:37 -0700

    little progress

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 
+
+
 app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.send('')
+})
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
